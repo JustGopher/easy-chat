9e87e305738d388bb3849d7c99cfff06ad207257
RationalDysaniaer <122082502+RationalDysaniaer@users.noreply.github.com>
2024-10-10 16:07:07 +0800

    美化服务端界面，添加显示用户列表功能

--- server/main.go
+++ server/main.go
@@ -12,7 +12,10 @@ var (
 )
 
 func main() {
-	fmt.Println("服务器开始监听...")
+	//起始界面
+	homeText()
+
+	//开始监听
 	listen, err := net.Listen("tcp", "localhost:8088")
 	if err != nil {
 		fmt.Println("listen err=,", err)
@@ -20,9 +23,10 @@ func main() {
 	}
 	defer listen.Close() //延时关闭
 
-	//循环等待客户端来连接我
+	//循环等待客户端的连接
+	fmt.Println("等待客户端连接")
+
 	for {
-		fmt.Println("等待客户端连接")
 		conn, err := listen.Accept()
 		if err != nil {
 			fmt.Println("Accept() err=", err)
@@ -33,6 +37,14 @@ func main() {
 		mu.Unlock()
 		//这里准备起一个协程
 		go process(conn)
+		println("---------------------------------------------------")
+		println("有客户端连接,客户端地址:", conn.RemoteAddr().String())
+		//当前所有用户
+		fmt.Println("当前用户列表：")
+		for n, v := range connections {
+			fmt.Printf("%v %v\n", n.RemoteAddr().String(), v)
+		}
+		println("---------------------------------------------------")
 	}
 
 	//fmt.Printf("listen suc=%v", listen)
@@ -42,20 +54,25 @@ func process(conn net.Conn) {
 	defer conn.Close()
 	defer func() {
 		mu.Lock()
-		delete(connections, conn) // 移除连接
+		//delete(connections, conn) // 移除连接
+		connections[conn] = false
 		mu.Unlock()
-		conn.Close()
+		println("---------------------------------------------------")
+		fmt.Println("当前用户列表：")
+		for n, v := range connections {
+			fmt.Printf("%v %v\n", n.RemoteAddr().String(), v)
+		}
+		println("---------------------------------------------------")
 	}()
 	//循环接收客户端发送的数据
 	for {
-		//创建一个新的切片
 		buf := make([]byte, 1024)
 		//等待客户端通过conn发送信息
 		//如果客户端没有write，那么协程就会阻塞在这里
 		//fmt.Printf("服务器在等待客户端%s发送信息\n", conn.RemoteAddr().String())
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println("err=", err)
+			fmt.Println("Read() err 该客户端连接已断开", conn.RemoteAddr().String())
 			return
 		}
 		//显示客户端发送的消息到终端
@@ -65,11 +82,35 @@ func process(conn net.Conn) {
 
 		// 广播消息
 		mu.Lock()
-		for c := range connections {
-			if c != conn { // 不发送给自己
+		for c, v := range connections {
+			if c != conn && v == true { // 不发送给自己和离线用户
 				_, _ = c.Write([]byte(message)) // 发送给其他客户端
 			}
 		}
 		mu.Unlock()
 	}
+}
+
+func homeText() {
+	clearConsole()
+	logo()
+	fmt.Println()
+	fmt.Println("服务开始监听...")
+}
+
+func logo() {
+	fmt.Printf(`╔═══╗─────────────╔═══╗╔╗───────╔╗─────╔═══╗
+║╔══╝─────────────║╔═╗║║║──────╔╝╚╗────║╔═╗║
+║╚══╗╔══╗╔══╗╔╗─╔╗║║─╚╝║╚═╗╔══╗╚╗╔╝────║║─╚╝╔══╗
+║╔══╝║╔╗║║══╣║║─║║║║─╔╗║╔╗║║╔╗║─║║─╔══╗║║╔═╗║╔╗║
+║╚══╗║╔╗║╠══║║╚═╝║║╚═╝║║║║║║╔╗║─║╚╗╚══╝║╚╩═║║╚╝║
+╚═══╝╚╝╚╝╚══╝╚═╗╔╝╚═══╝╚╝╚╝╚╝╚╝─╚═╝────╚═══╝╚══╝
+─────────────╔═╝║─────by:RationalDysaniaer
+─────────────╚══╝ 
+`)
+}
+
+func clearConsole() {
+	fmt.Print("\033[2J\033[3J") // 清除屏幕
+	fmt.Print("\033[H")         // 将光标移动到左上角
 }
